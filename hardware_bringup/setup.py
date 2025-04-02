4cae93dd595af9aeb3174a1afdbe0e459e71cd40
jun <junyeong4321@gmail.com>
2025-04-02 17:19:09 +0900

    Modified package name, and setup.py to build well

--- hardware_bringup/setup.py
+++ hardware_bringup/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'hardware_bringup'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # launch 파일 설치 설정
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
