8eab8f215a1216233d645a8d728d6940011c677e
iamjunyeong <junyeong4321@gmail.com>
2025-04-29 11:45:11 +0900

    Added basic setting for ekf filter node global

--- hardware_bringup/launch/localization.launch.py
+++ hardware_bringup/launch/localization.launch.py
@@ -1,6 +1,4 @@
 # Copyright 2018 Open Source Robotics Foundation, Inc.
-# Copyright 2019 Samsung Research America
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,7 +12,6 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 import os
 import yaml
@@ -22,14 +19,51 @@ from launch.substitutions import EnvironmentVariable
 import pathlib
 import launch.actions
 from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    robot_localization_dir = get_package_share_directory('robot_localization')
+    parameters_file_dir = os.path.join(robot_localization_dir, 'params')
+    parameters_file_path = os.path.join(parameters_file_dir, 'dual_ekf_navsat_example.yaml')
+    os.environ['FILE_PATH'] = str(parameters_file_dir)
     return LaunchDescription([
-        launch_ros.actions.Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
-            output='screen',
-            parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
+        launch.actions.DeclareLaunchArgument(
+            'output_final_position',
+            default_value='false'),
+        launch.actions.DeclareLaunchArgument(
+            'output_location',
+	    default_value='~/dual_ekf_navsat_example_debug.txt'),
+	
+    launch_ros.actions.Node(
+            package='robot_localization', 
+            executable='ekf_node', 
+            name='ekf_filter_node_odom',
+	        output='screen',
+            parameters=[parameters_file_path],
+            remappings=[('odometry/filtered', 'odometry/local')]           
            ),
+    launch_ros.actions.Node(
+            package='robot_localization', 
+            executable='ekf_node', 
+            name='ekf_filter_node_map',
+	        output='screen',
+            parameters=[parameters_file_path],
+            remappings=[('odometry/filtered', 'odometry/global')]
+           ),           
+    launch_ros.actions.Node(
+            package='robot_localization', 
+            executable='navsat_transform_node', 
+            name='navsat_transform',
+	        output='screen',
+            parameters=[parameters_file_path],
+            arguments=['--ros-args', '--log-level', 'navsat_transform:=debug'], 
+            remappings=[('imu', '/imu/data'),
+                        ('gps/fix', '/ublox_gps_node/fix'), 
+                        #('gps/filtered', 'gps/filtered'),
+                        ('odometry/gps', 'odometry/gps'),
+                        #('odometry/filtered', 'odometry/local'),
+                        ('odometry/filtered', 'odometry/global'),
+                    ]           
+
+           )           
 ])
