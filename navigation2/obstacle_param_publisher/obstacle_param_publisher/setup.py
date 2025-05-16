63cf54f196379b2b5aa9a67180ea40359f21b4b4
jihun <ll1891@naver.com>
2025-05-16 13:42:15 +0900

    obstacle_param_publisher updated

--- navigation2/obstacle_param_publisher/obstacle_param_publisher/setup.py
+++ navigation2/obstacle_param_publisher/obstacle_param_publisher/setup.py
@@ -22,6 +22,8 @@ setup(
         'console_scripts': [
             'obstacle_param_publisher = obstacle_param_publisher.obstacle_param_publisher:main',  # ✅ 유지
             'keyboard_obstacle_publisher = obstacle_param_publisher.keyboard_obstacle_publisher:main', # ✅ 추가
+            'lidar_processor = obstacle_param_publisher.lidar_processor:main',  # ✅ 추가
+            'obstacle_integration_node = obstacle_param_publisher.obstacle_integration_node:main',  # ✅ 추가
         ],
     },
 )
